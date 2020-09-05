6cd15ce7dbd59936da114d0e688aa68060b5c0da
Leandro <lsmarques@gmail.com>
2020-09-04 22:37:01 -0300

    Vai

---
+++ build/precache-manifest.c927655158f3c264c8c618526cecdf11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9db5f8515c28f03198b2619ff35bec47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a91c4173756ae5fbc64d",
+    "url": "/static/css/main.813ee94c.chunk.css"
+  },
+  {
+    "revision": "20596330febdf6cef57e",
+    "url": "/static/js/2.f80305ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f80305ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a91c4173756ae5fbc64d",
+    "url": "/static/js/main.b7b0fa20.chunk.js"
+  },
+  {
+    "revision": "49061e827ba46d446f5e",
+    "url": "/static/js/runtime-main.fa93b6e6.js"
+  }
+]);
